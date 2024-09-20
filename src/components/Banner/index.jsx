5e4e0c0f5de4c1ea29c614f1a8ef3f8452046adc
Yannstj <yannick.saintjust@gmail.com>
2024-09-20 09:13:10 +0200

    add banner img jpeg format

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,11 @@
 import '../../styles/banner.scss'
-import bannerImage from '../../assets/porco_rosso.webp'
+//import bannerImage from '../../assets/porco_rosso.webp'
 
 function Banner() {
     return(
        <section className="bannerWrapper">
         <div className="banner">
-            <img src={bannerImage} alt="en recherche d'inspi" className="banner__background"></img>
+            {/* <img src={bannerImage} alt="en recherche d'inspi" className="banner__background"></img> */}
             <h1 className="banner__title">Chez vous partous ailleurs</h1>
         </div>
        </section>
