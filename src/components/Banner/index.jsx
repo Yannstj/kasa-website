38a2069349947983a53633d3f3d884e5ebd32a55
Yannstj <yannick.saintjust@gmail.com>
2024-09-29 20:27:30 +0200

    homepage CSS no error

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,12 +2,14 @@ import '../../styles/banner.scss'
 
 function Banner() {
   return (
-    <section className="bannerWrapper">
-      <div className="banner">
-        {/* <img src={bannerImage} alt="en recherche d'inspi" className="banner__background"></img> */}
-        <h1 className="banner__title">Chez vous partous ailleurs</h1>
-      </div>
-    </section>
+    <div className="bannerBox">
+      <section className="bannerWrapper">
+        <div className="banner">
+          {/* <img src={bannerImage} alt="en recherche d'inspi" className="banner__background"></img> */}
+          <h1 className="banner__title">Chez vous partous ailleurs</h1>
+        </div>
+      </section>
+    </div>
   )
 }
 
