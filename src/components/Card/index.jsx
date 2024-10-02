f210381cdd2760e6e09d3e65cf64ad2d65dd67cc
Yannstj <yannick.saintjust@gmail.com>
2024-10-02 20:13:52 +0200

    replace section tag by figure and figcaption plus set cover and title in cards elements

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,34 @@
 import { logementList } from '../../data/logementList'
 import '../../styles/card.scss'
 
+// function Card() {
+//   return (
+//     <div className="cardBox">
+//       <figure className="cardWrapper">
+//         {logementList.map((logement) => (
+//           <div className="card" key={logement.id}>
+//             <img src={logement.cover} alt="tempAlt" className="card__cover" />
+//             <figcaption className="card__title">{logement.title}</figcaption>
+//           </div>
+//         ))}
+//       </figure>
+//     </div>
+//   )
+// }
+
 function Card() {
   return (
     <div className="cardBox">
       <section className="cardWrapper">
         {logementList.map((logement) => (
-          <div className="card" key={logement.id}>
-            <h2 className="card__title">{logement.title}</h2>
-          </div>
+          <figure className="card" key={logement.id}>
+            <img src={logement.cover} alt="tempAlt" className="card__cover" />
+            <figcaption className="card__title">{logement.title}</figcaption>
+          </figure>
         ))}
       </section>
     </div>
   )
 }
+
 export default Card
