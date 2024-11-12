49b8ea83259a21fd4475116d66765aa4c8065048
Yannstj <yannick.saintjust@gmail.com>
2024-11-12 17:25:49 +0100

    collapsz

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,13 +1,20 @@
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import '../../styles/collapse.scss'
 
 function Collapse({ collapseContent }) {
+  const [isOpen, setIsOpen] = useState({})
+
+  const toggleCollapse = (key) => {
+    setIsOpen((prev) => ({ ...prev, [key]: !prev[key] }))
+  }
+
   return (
     <div>
       {collapseContent.map(({ title, content, key }) => (
-        <details className="collapse" key={key}>
+        <details className="collapse" key={key} open={setIsOpen[key]}>
           <summary
             className="collapse__title"
             onClick={(e) => e.preventDefault()}
@@ -15,14 +22,24 @@ function Collapse({ collapseContent }) {
             <h2>{title}</h2>
             <span
               className="collapse__button"
-              onClick={(e) => e.stopPropagation()}
+              onClick={(e) => {
+                e.stopPropagation()
+                toggleCollapse(key)
+              }}
             >
-              <FontAwesomeIcon icon={faChevronUp} className="collapse__icon" />
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                className={`collapse__icon ${
+                  isOpen[key] ? 'collapse__icon__rotate' : ''
+                }`}
+              />
             </span>
           </summary>
-          <div className="collapse__content">
-            <p>{content}</p>
-          </div>
+          {isOpen[key] && (
+            <div className="collapse__content">
+              <p>{content}</p>
+            </div>
+          )}
         </details>
       ))}
     </div>
