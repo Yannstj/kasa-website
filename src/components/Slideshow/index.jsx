f567cb2ce21bd1529f5c73b0a2ec4c750278e8e3
Yannstj <yannick.saintjust@gmail.com>
2024-11-14 00:38:10 +0100

    chevron arrow in carrousel v1

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,5 +1,8 @@
 import { useLocation } from 'react-router-dom'
 // import Collapse from '../../components/Collapse'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import '../../styles/logement.scss'
 
 function Slideshow({ logement }) {
@@ -12,8 +15,17 @@ function Slideshow({ logement }) {
           currentlocation.pathname.includes(`/logement/${id}`) ? (
             <div className="carrousel">
               <div className="carrousel__image" key={id}>
+                <FontAwesomeIcon
+                  icon={fas.faChevronLeft}
+                  className="carrousel__left__arrow"
+                />
+                <FontAwesomeIcon
+                  icon={fas.faChevronRight}
+                  className="carrousel__right__arrow"
+                />
                 <img src={cover} alt={title} />
               </div>
+
               <h1>{title}</h1>
               <h2>{location}</h2>
               <ul className="carrousel__tags">
