353ffad6062e529301d8b780e7fab793ac0aee2c
Yannstj <yannick.saintjust@gmail.com>
2024-11-13 22:36:24 +0100

    using pathlocation for matching id logement and styled slideshow component

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,18 +1,27 @@
+import { useLocation } from 'react-router-dom'
+// import Collapse from '../../components/Collapse'
 import '../../styles/logement.scss'
 
 function Slideshow({ logement }) {
+  const currentlocation = useLocation()
+
   return (
     <main className="slideshowContainer">
       <section>
-        <div className="carrousel">
-          {logement.map(({ id, title, cover }) => (
-            <div className="carrousel__image" key={id}>
-              {' '}
-              <img src={cover} />
+        {logement.map(({ id, title, cover, location, tags }) =>
+          currentlocation.pathname.includes(`/logement/${id}`) ? (
+            <div className="carrousel">
+              <div className="carrousel__image" key={id}>
+                <img src={cover} alt={title} />
+              </div>
               <h1>{title}</h1>
+              <h2>{location}</h2>
+              <ul className="carrousel__tags">
+                <li>{tags}</li>
+              </ul>
             </div>
-          ))}
-        </div>
+          ) : null
+        )}
       </section>
     </main>
   )
